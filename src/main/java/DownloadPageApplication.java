2bd8e12b4838f37a5a83fcaad3d64de5c8ae84b7
Jaras49 <jaras48@gmail.com>
2018-05-11 20:05:42 +0200

    commit

--- src/main/java/DownloadPageApplication.java
+++ src/main/java/DownloadPageApplication.java
@@ -29,7 +29,7 @@ public class DownloadPageApplication {
         );
 
         pageDownloader.downloadPage(
-                pageSnapshotCreator.createPageNode(new UrlNormalizer().normalize("https://www.fbi.gov/"), 2),
+                pageSnapshotCreator.createPageNode(new UrlNormalizer().normalize("https://www.iana.org/"), 2),
                 tempDirectory);
     }
 
