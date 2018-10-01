cb94a2e1426faed570a8c21693ce14a7e960dd62
Jaras49 <jaras48@gmail.com>
2018-10-01 11:03:12 +0200

    Split HTML code from HttpPageExtractorTest class to html file

--- src/test/java/com/web/crawler/extract/HttpPageExtractorTest.java
+++ src/test/java/com/web/crawler/extract/HttpPageExtractorTest.java
@@ -3,70 +3,29 @@ package com.web.crawler.extract;
 import com.web.crawler.model.Page;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 import static org.junit.Assert.*;
 
 public class HttpPageExtractorTest {
 
-
-
     @Test
-    public void shouldExtractPage() {
+    public void shouldExtractPage() throws URISyntaxException, IOException {
 
         //Given
         String link = "http://example.com/";
-        String source = "<!doctype html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "    <title>Example Domain</title>\n" +
-                "\n" +
-                "    <meta charset=\"utf-8\" />\n" +
-                "    <meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n" +
-                "    <style type=\"text/css\">\n" +
-                "    body {\n" +
-                "        background-color: #f0f0f2;\n" +
-                "        margin: 0;\n" +
-                "        padding: 0;\n" +
-                "        font-family: \"Open Sans\", \"Helvetica Neue\", Helvetica, Arial, sans-serif;\n" +
-                "        \n" +
-                "    }\n" +
-                "    div {\n" +
-                "        width: 600px;\n" +
-                "        margin: 5em auto;\n" +
-                "        padding: 50px;\n" +
-                "        background-color: #fff;\n" +
-                "        border-radius: 1em;\n" +
-                "    }\n" +
-                "    a:link, a:visited {\n" +
-                "        color: #38488f;\n" +
-                "        text-decoration: none;\n" +
-                "    }\n" +
-                "    @media (max-width: 700px) {\n" +
-                "        body {\n" +
-                "            background-color: #fff;\n" +
-                "        }\n" +
-                "        div {\n" +
-                "            width: auto;\n" +
-                "            margin: 0 auto;\n" +
-                "            border-radius: 0;\n" +
-                "            padding: 1em;\n" +
-                "        }\n" +
-                "    }\n" +
-                "    </style>    \n" +
-                "</head>\n" +
-                "\n" +
-                "<body>\n" +
-                "<div>\n" +
-                "    <h1>Example Domain</h1>\n" +
-                "    <p>This domain is established to be used for illustrative examples in documents. You may use this\n" +
-                "    domain in examples without prior coordination or asking for permission.</p>\n" +
-                "    <p><a href=\"http://www.iana.org/domains/example\">More information...</a></p>\n" +
-                "</div>\n" +
-                "</body>\n" +
-                "</html>\n";
 
+        URL resource = this.getClass().getClassLoader().getResource("html/index.html");
 
-        Page page = new Page(link, source);
+        String body = Files.readAllLines(Paths.get(resource.toURI())).stream()
+                .collect(Collectors.joining("\n"));
+
+        Page page = new Page(link, body);
         PageExtractor pageExtractor = new HttpPageExtractor();
 
         //When
